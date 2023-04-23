2c158e588e86aae3838923c082d44bbda1a1ae32
Jay Abdal <jay.abdal@outlook.com>
2023-04-23 10:39:02 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using cron_yaml;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace YourNamespace
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<BackgroundTaskService>();
+                });
+        }
+    }
+}
